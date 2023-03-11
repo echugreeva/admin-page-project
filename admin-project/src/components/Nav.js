211fb6be2ff9f207a4e276d4f58657471ab406ca
echugreeva <e.chugreeva@gmail.com>
2023-03-11 09:01:03 +0200

    component filess

---
+++ admin-project/src/components/Nav.js
@@ -0,0 +1,11 @@
+const Nav = () => {
+    return (
+        <div>
+            <div>Login</div>
+            <div>Users</div>
+            <div></div>
+        </div>
+    )
+}
+
+export default Nav
